f490258523eb4eeed4c41dc4a1ddf91b9fc0deee
NAVIN CHAUDHARY <154901815+Navin-Chaudhary@users.noreply.github.com>
2024-07-22 00:25:53 +0530

    ToDo List

--- script.js
+++ script.js
@@ -1,51 +1,95 @@
+let todos = [];
 let button = document.querySelector("#addTaskBtn");
-button.addEventListener("click", () => {
-    let taskinput = document.querySelector("#taskInput").value.trim();
-    let tasklist = document.querySelector("#taskList");
-
-    function addtodo() {
-        if (taskinput != '') {
-            let newli = document.createElement('li');
-            newli.innerText = taskinput;
+let tasklist = document.querySelector("#taskList");
+let form=document.querySelector('form');
+form.addEventListener('submit', () => {
+    let input=document.querySelector("#taskInput")
+    let taskinput = input.value.trim();
+    // if(todos.find(e=>e.input==taskinput)) return
+    function addtodos() {
+        if (taskinput !== '') {
             
-            
-            let checkbtn = document.createElement('input');
-            checkbtn.className = 'checkbox';
-            checkbtn.type = 'checkbox';
-            newli.appendChild(checkbtn);
-            
-
-            let icon = document.createElement('i');
-            icon.className = 'fa-solid fa-trash';
-            icon.addEventListener('click', () => {
-                tasklist.removeChild(newli);
-                tasklist.removeChild(checkbtn)
+            todos.push({
+                id: Math.random().toString(),
+                input: taskinput,
+                isCompleted: false
             });
+            
+            addToDom(todos);
+            localStorage.setItem('todos', JSON.stringify(todos));
+        }
+        
+    }
+    addtodos();
+    input.value=""
+});
+function clearcompleted(){
     
-            newli.appendChild(icon);
+        todos=todos.filter((e)=>{
+            
+            return !e.isCompleted
+        })
+        tasklist.innerHTML=""
+        addToDom(todos)
+        localStorage.setItem('todos', JSON.stringify(todos));
+        
+}
+function addToDom(arr) {
+    arr.forEach(task => {
+        let newli = document.createElement('li');
+        let div = document.createElement('div');
+        div.innerText = task.input;
+        newli.appendChild(div)
+        newli.id = task.id;
 
-            tasklist.appendChild(newli);
+        if (task.isCompleted) {
+            newli.style.textDecoration = "line-through";
+        } else {
+            newli.style.textDecoration = "none";
+        }
 
-            checkbtn.addEventListener('click', () => {
+        let checkbtn = document.createElement('input');
+        checkbtn.className = 'checkbox';
+        checkbtn.type = 'checkbox';
+        checkbtn.checked = task.isCompleted;
+        div.prepend(checkbtn);
 
-                if (checkbtn || checkbtn.checked) {
-                    let completedTasks = document.querySelector("#completedTasks");
-                    completedTasks.appendChild(newli);
-                    tasklist.removeChild(checkbtn);
-                }
-            });
-            checkbtn.addEventListener('click',()=>{
-                newli.style.textDecoration = "line-through";
-                if(newli.style.textDecoration = "line-through"){
-                    checkbtn.addEventListener('click',()=>{
-                        newli.style.textDecoration = "none";
-                        
-                    })
-                }
-            })
+        checkbtn.addEventListener('change', () => {
+            todos = todos.map(todo => todo.id === task.id ? { ...todo, isCompleted: !task.isCompleted } : todo);
+            tasklist.innerHTML = "";
+            addToDom(todos)
+            localStorage.setItem('todos', JSON.stringify(todos));
+        });
+
+
+        let icon = document.createElement('i');
+        icon.className = 'fa-solid fa-trash';
+        icon.addEventListener('click', () => {
+            tasklist.removeChild(newli);
+            todos = todos.filter(t => t.id !== task.id);
+            localStorage.setItem('todos', JSON.stringify(todos));
+        });
+        newli.appendChild(icon);
+        
+        if ([...tasklist.childNodes].find(cn => cn.id === task.id)) return
+        tasklist.appendChild(newli);
+
+    });
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const savedTODOS = localStorage.getItem("todos");
+    if (savedTODOS) {
+        const JSONTODOS = JSON.parse(savedTODOS);
+        if (!Array.isArray(JSONTODOS)) {
+            return
 
         }
+        todos = JSONTODOS;
+        addToDom(JSONTODOS);
     }
-    addtodo();
 });
+
 
